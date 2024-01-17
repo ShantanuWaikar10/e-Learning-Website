2bae07695d2bfd42d338eb20e969ec567203d407
Shantanu Waikar <89289274+ShantanuWaikar10@users.noreply.github.com>
2024-01-17 12:00:24 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -31,7 +31,7 @@ const VideoPlayer = () => (
             controlsList='nodownload nofullscreen noremoteplayback'
             disableRemotePlayback
             disablePictureInPicture
-            src="https://res.cloudinary.com/drrl6fxgi/video/upload/v1675687285/f1zayeqrvsysheoakilp.mp4">
+            src="https://res.cloudinary.com/drrl6fxgi/video/upload/v1705472039/spvsd0t4ljbgn0kblwcr.mp4">
         </video>
     </Box>
 )
